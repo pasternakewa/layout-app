0ddcdb78f7022dc6f2e99a49dc728652c5d5cf29
pasternakewa <ewapasternak.kontakt@gmail.com>
2020-12-23 00:10:47 +0100

    Add clothes categories images gallery

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function Image({ title }) {
-  return <img src={title} alt={title}></img>;
+export default function Image({ title, elementNumber }) {
+  return (
+    <figure
+      className={`app__categories__gallery--1__element--${elementNumber}`}
+    >
+      <img src={title} alt={title}></img>
+    </figure>
+  );
 }
