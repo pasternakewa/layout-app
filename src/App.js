b2f30d8efc79645792687bd82d723574dc17a05a
pasternakewa <ewapasternak.kontakt@gmail.com>
2021-01-06 19:18:45 +0100

    Add routing for nav links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from "react"
+import React from "react";
 import "./App.css";
-import Nav from "./components/Nav"
-import Products from "./components/Products"
-import About from "./components/About"
+import Nav from "./components/Nav";
+import Products from "./components/Products";
+import About from "./components/About";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Products />
-      <About />
-    </div>
-      )
-    }
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/products" component={Products} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
     <nav>
       <h2>MAKEUP SHOWROOM</h2>
       <ul className="nav-links">
-        <li>Products</li>
-        <li>About</li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
       </ul>
     </nav>
   );
