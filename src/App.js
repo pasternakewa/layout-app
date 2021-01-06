5116e1f4d7b9a2fff69550246b5c47a4f35fc72f
pasternakewa <57099208+pasternakewa@users.noreply.github.com>
2021-01-06 16:31:57 +0100

    Dsiplay app pages contents in main app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react"
 import "./App.css";
+import Nav from "./components/Nav"
+import Products from "./components/Products"
+import About from "./components/About"
 
 function App() {
-  return <div className="App"></div>;
-}
+  return (
+    <div className="App">
+      <Nav />
+      <Products />
+      <About />
+    </div>
+      )
+    }
 
 export default App;
