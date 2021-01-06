9be0fb226166e939b4e3abb3cb1b35d3730610de
pasternakewa <ewapasternak.kontakt@gmail.com>
2021-01-06 15:10:39 +0100

    Remove unused code for app restructure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import "./App.css";
-import Image from "./components/Image";
-import shoes from "./images/shoes.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="app__categories">
-        <div className="app__categories__gallery--1">
-          <Image title={shoes} elementNumber="1" />
-          <Image title={shoes} elementNumber="2" />
-          <Image title={shoes} elementNumber="3" />
-          <Image title={shoes} elementNumber="4" />
-          <Image title={shoes} elementNumber="5" />
-          <Image title={shoes} elementNumber="6" />
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
