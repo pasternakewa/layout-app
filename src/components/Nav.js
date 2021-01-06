2fef556707b037c9c050f0d82a4343758be4a12f
pasternakewa <ewapasternak.kontakt@gmail.com>
2021-01-06 16:06:57 +0100

    Add nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav>
+      <h2>MAKEUP SHOWROOM</h2>
+      <ul>
+        <li>Products</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
