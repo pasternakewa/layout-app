0e6abca5bf7010f1dbad0cee12c6ac955769c297
ewa <ewapasternak.kontakt@gmail.com>
2021-01-28 19:45:18 +0100

    Add paragraph dividers in About section for smallest devices

--- src/components/About.js
+++ src/components/About.js
@@ -11,8 +11,10 @@ export default function About() {
       <div className="about-section">
         <Image src={brushes} name="brushes" className="about-section__img" />
         <p className="about-section__text">You'll find more than 15,000 products from approximately 450 beauty brands across all categories and price points. Our core values are diversity, courage and eco-friendliness.</p>
+        <hr />
         <Image src={recycle} name="recycle" className="about-section__img" />
         <p className="about-section__text">We design, develop and manufacture injection moulded reusable packaging. Our products offer includes solutions for every market such as durable beverage crates, pallets, handling trays and bulk-containers. A pioneer in fit-to-purpose loop ready packaging throughout the entire supply chain.</p>
+        <hr />
         <Image src={palette} name="palette" className="about-section__img" />
         <p className="about-section__text">To you, brave human, fighter who struggles every day to wear your smile like a flag. To you, the one whose eyes are the colour of strength, the eyes with which you transmit your gaze, the sensitivity that you feel towards life and towards everyone around you. We create cosmetics for you because nobody else knows how to use every battle to continue through the storm. Be creative and wear your make-up proudly.</p>
       </div>
