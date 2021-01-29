396ae0037defff25083e8aa08ee973f34582f9ad
ewa <ewapasternak.kontakt@gmail.com>
2021-01-29 23:37:20 +0100

    Display messages for possible fetching errors

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import GalleryImage from "./GalleryImage";
+import { Link } from "react-router-dom";
 
 export default function Products() {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(null)
 
   useEffect(() => {
     fetchItems();
@@ -13,27 +15,27 @@ export default function Products() {
   const fetchItems = async () => {
     try {
       const fetchedData = await fetch(
-        "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+        "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
       );
       const items = await fetchedData.json();
       setItems(items);
       setIsLoading(false);
     } catch (e) {
-      console.log("error:", e);
+      setIsError(e.toString())
       setIsLoading(false);
     }
   };
 
   let getUniqueCategoryProducts = [
-    ...new Map(items.map((item) => [item.product_type, item])).values()
+    ...new Map(items?.map((item) => [item.product_type, item])).values()
   ];
-
   return (
     <div className="gallery-container">
       <h1 className="gallery-title">Products</h1>
+        {(isError || (!isLoading && items.length===0)) && <p>Problem loading data. {isError} Get back to <Link to="/">home page</Link>.</p>}
       <div className="gallery">
         {isLoading && <h4>Loading products...</h4>}
-        {getUniqueCategoryProducts.map((item) => (
+        {getUniqueCategoryProducts?.map((item) => (
           <GalleryImage
             name={item.name}
             src={item.image_link}
