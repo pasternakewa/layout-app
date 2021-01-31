d98dfe0e7417c4f68fc949f9fd768bec078767e2
ewa <ewapasternak.kontakt@gmail.com>
2021-01-31 22:49:06 +0100

    Format components to arrow functions and fix overall formatting

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Navigation() {
-  return (
-    <div className="nav-container">
-      <nav>
-        <p className="title">MAKEUP SHOWROOM</p>
-        <ul className="nav-links">
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-}
+const Navigation = () => (
+  <div className="nav-container">
+    <nav>
+      <p className="title">MAKEUP SHOWROOM</p>
+      <ul className="nav-links">
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Navigation;
