75791e9af237c9fb557486ef84c707e3a366068a
ewa <ewapasternak.kontakt@gmail.com>
2021-01-31 22:53:01 +0100

    Fix components' formatting

--- src/components/GalleryImage.js
+++ src/components/GalleryImage.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function GalleryImage({ category, id, name, src }) {
-  return (
-    <figure className="gallery-item" key={id}>
-      <img className="gallery-item__img" src={src} alt={name}></img>
-      <figcaption className="gallery-item__img-caption">{category}</figcaption>
-    </figure>
-  );
-}
+const GalleryImage = ({ category, id, name, src }) => (
+  <figure className="gallery-item" key={id}>
+    <img className="gallery-item__img" src={src} alt={name}></img>
+    <figcaption className="gallery-item__img-caption">{category}</figcaption>
+  </figure>
+);
+
+export default GalleryImage;
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const Image = ({src, name, className})=> {
-    return (
-        <div >
-            <img src={src} alt={name} className={className} />
-        </div>
-    )
-}
+const Image = ({ src, name, className }) => {
+  return (
+    <div>
+      <img src={src} alt={name} className={className} />
+    </div>
+  );
+};
 
-export default Image
+export default Image;
