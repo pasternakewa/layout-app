205e0edf4ea13cb60d2d7e053a802f8f6f70a718
ewa <ewapasternak.kontakt@gmail.com>
2021-02-07 00:20:00 +0100

    Make requested changes

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-const Image = ({ src, name, className }) => {
-  return (
-    <div>
-      <img src={src} alt={name} className={className} />
-    </div>
-  );
-};
+const Image = ({ src, name, className }) => (
+  <div>
+    <img src={src} alt={name} className={className} />
+  </div>
+);
 
 export default Image;
